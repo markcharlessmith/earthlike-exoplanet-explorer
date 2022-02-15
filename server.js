4a2e6a7d0fce146cad6399ae42cb9181a9ab75ae
Mark Charles Smith <markcharlessmith@gmail.com>
2022-02-15 16:16:17 -0500

    created core files

---
+++ server.js
@@ -0,0 +1,8 @@
+// server boilerplate
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+// listens on port 3000
+app.listen(3000); // http://localhost:3000/
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HWP = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, '/src/index.js'),
+    output: {
+        filename: 'build.js',
+        path: path.join(__dirname, '/dist')},
+        module: {
+            rules:[{
+            // test: /\.js$/,
+            test: /\.(js|jsx)$/
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins:[
+        new HWP(
+            {template: path.join(__dirname,'/src/index.html')}
+        )
+    ]
+}
