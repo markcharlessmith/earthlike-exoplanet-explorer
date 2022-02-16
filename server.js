3366450321bf26549f96d96d30b258e785c614e9
Mark Charles Smith <markcharlessmith@gmail.com>
2022-02-15 22:01:51 -0500

    updated webpack.config, package.json

--- server.js
+++ server.js
@@ -2,7 +2,31 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const PORT = 3000;
 
+// import controller (will I have a controller)
+// const controller = require('./controller');
 
-// listens on port 3000
-app.listen(3000); // http://localhost:3000/
+// JSON parse all incoming requests
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World');
+});
+
+// Global error handling 
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 500,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
+
+// server listens on port 3000
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+}); // http://localhost:3000/
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,40 @@
-const path = require('path');
-const HWP = require('html-webpack-plugin');
+const path = require("path");
+const HWP = require("html-webpack-plugin");
 module.exports = {
-    mode: 'development',
-    entry: path.join(__dirname, '/src/index.js'),
-    output: {
-        filename: 'build.js',
-        path: path.join(__dirname, '/dist')},
-        module: {
-            rules:[{
-            // test: /\.js$/,
-            test: /\.(js|jsx)$/
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        }]
+  mode: "development",
+  entry: path.join(__dirname, "/src/index.js"),
+  output: {
+    filename: "build.js",
+    path: path.join(__dirname, "/dist"),
+  },
+  // publicPath: '/',
+  module: {
+    rules: [
+      {
+        // test: /\.js$/,
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+    proxy: {
+      "/api**": {
+        target: "http://localhost:3000/",
+        secure: false,
+      },
+      "/assets/**": {
+        target: "http://localhost:3000/",
+        secure: false,
+      },
     },
-    plugins:[
-        new HWP(
-            {template: path.join(__dirname,'/src/index.html')}
-        )
-    ]
-}
+  },
+  plugins: [new HWP({ template: path.join(__dirname, "/src/index.html") })],
+};
