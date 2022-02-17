c3a9c94c99184d31133297c4f8a3296705be3332
Mark Charles Smith <markcharlessmith@gmail.com>
2022-02-17 00:02:46 -0500

    added App.jsx

---
+++ components/App.jsx
@@ -0,0 +1,12 @@
+// import React, { Component } from 'react';
+const React = require('react');
+
+class App extends React.Component {
+    render() {
+    return (
+        <h1>Hello Mark</h1>
+    );
+    }
+}
+
+module.exports = App;
