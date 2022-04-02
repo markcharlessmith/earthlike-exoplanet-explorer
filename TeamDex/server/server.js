b15a304c7caacb33b2d38dad8172e3095cbb1b31
Mark Charles Smith <markcharlessmith@gmail.com>
2022-04-02 08:47:17 -0400

    added basic file structure

---
+++ TeamDex/server/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+module.exports = app;
---
+++ TeamDex/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import { CssBaseline } from '@mui/material';
+
+function App() {
+  
+  return(
+    <div>
+    <h1>hello world</h1>
+    </div>
+  );
+}
+export default App;
